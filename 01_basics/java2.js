f6748ae014b9902621dd354add728903c809a6a9
sniteshg <151628440+sniteshg@users.noreply.github.com>
2023-11-22 05:24:04 +0000

    conversion of data type

---
+++ 01_basics/java2.js
@@ -0,0 +1,11 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+console.log(typeof(ValueInNumber));
+console.log(ValueInNumber)
+
+// "33" => 33
+// '33abc' is not convertable
+// 
